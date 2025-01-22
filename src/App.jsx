0255a7cd832f4a731af12b4cfd23c3bf52c8a676
Priya121404 <priyachauhan121404@gmail.com>
2025-01-22 19:29:10 +0530

    path

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import {Routes,Route} from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Coin from './pages/Coin/Coin'
-import Footer from './components/Footer/Footer'
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Coin from './pages/Coin/Coin';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path='/CryptoPlace/'  element={<Home/>}/>
-        <Route path='/CryptoPlace/coin/:coinId' element={<Coin/>}/>
-      </Routes>
-      <Footer/>
-    </div>
-  )
+    <Router basename="/CryptoPlace"> {/* Set basename here */}
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/coin/:coinId" element={<Coin />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
